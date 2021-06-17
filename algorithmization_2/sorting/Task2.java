55f421eebf9778e68ed03092b17e1b18aaea51e6
Misha Dudko <dudkomikhail01@gmail.com>
2021-06-17 13:38:03 +0300

    fix conditions

--- algorithmization_2/sorting/Task2.java
+++ algorithmization_2/sorting/Task2.java
@@ -3,7 +3,7 @@ package sorting;
 import java.util.Scanner;
 
 /*
-    2. Даны две последовательности a1 < a2 < ... < an и b1 < b2 < ... < bm.
+    2. Даны две последовательности a1 <= a2 <= ... <= an и b1 <= b2 <= ... <= bm.
     Образовать из них новую последовательность чисел так, чтобы она тоже была неубывающей.
     Примечание. Дополнительный массив не использовать.
 */
@@ -26,22 +26,14 @@ public class Task2 {
             System.out.print("[" + i + "] = ");
             second[i] = in.nextInt();
         }
-
+        in.close();
+        
         int firstIterator = 0;
         int secondIterator = 0;
         System.out.println("\nИтоговый массив");
         while (firstIterator + secondIterator < first.length + second.length) {
-            if (firstIterator >= first.length) {
-                System.out.print(second[secondIterator] + " ");
-                secondIterator++;
-                continue;
-            }
-            if (secondIterator >= second.length) {
-                System.out.print(first[firstIterator] + " ");
-                firstIterator++;
-                continue;
-            }
-            if (first[firstIterator] < second[secondIterator]) {
+            if (secondIterator == second.length
+                    || (firstIterator != first.length && first[firstIterator] < second[secondIterator])) {
                 System.out.print(first[firstIterator] + " ");
                 firstIterator++;
             }
